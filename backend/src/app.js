67c4c1579dcc6b6d40bcc21738e5a5cc590cb3dd
Rahavan <rahar.navicode@gmail.com>
2026-08-08 12:02:40 +0530

    chore: initialize full stack project

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Community Project API is running"
+    });
+});
+
+module.exports = app;
